660b8b90c716d59f911d043d0a8225e507a33dc5
JOSIAH E MODESTE <ephraimmo2297@gmail.com>
2020-01-12 17:46:42 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,6 @@
+board= [" "," "," "," "," "," "," "," "]
+board[1]="X"
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -6,4 +9,14 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+def update_array_at_with(array,index,value)
+  array[index]= value
+end
+
+def input_to_index(move)         # code your input_to_index and move method here!
+  move.to_i - 1
+end
+
+def move(board,position,character="X")
+  board[position] = character
+end
